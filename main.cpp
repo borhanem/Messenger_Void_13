e07ba3958c1f4f43990911784bc3f13522b39b2c
MahtaMirzaei <mahtamirzaei1383@gmail.com>
2023-06-06 14:58:15 +0430

    theme.1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include "mainwindow.h"
 #include "loginpage.h"
 #include <QApplication>
+#include <QFile>
+
+QString read(QString path)
+{
+    QFile file(path);
+    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(&file);
+        return in.readAll();
+    }
+    return "";
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    int d =1;
+    QString css = read(QString(":/new/prefix1/Style/Style"  ".css"));
+
+    if( css.length() > 0)
+    {
+        a.setStyleSheet(css);
+    }
+
     LoginPage lpg;
     lpg.show();
 
