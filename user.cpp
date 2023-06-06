c5f84e9a6c0cdfa9f215c67e82252f3f7b9fd60d
Borhan <bmoyassari@gmail.com>
2023-06-06 14:49:00 +0330

    stage 1 implementation of user class(file system)

--- user.cpp
+++ user.cpp
@@ -1,8 +1,45 @@
 #include "user.h"
+#include <fstream>
+#include <iostream>
 
 
 User::User(QString userName, QString passWord, QString token, QObject *parent)
     : QObject{parent},m_username(userName),m_password(passWord),m_token(token)
 {
 
+}
+
+void User::signUp(char* filePath)
+{
+    writeToFile(filePath);
+
+
+
+    // do stuff later cuz this function is so stupid
+}
+
+void User::login()
+{
+
+}
+
+void User::logOut(char* filePath)
+{
+    std::remove(filePath);
+}
+
+void User::writeToFile(char *filePath)
+{
+    std::ofstream f1out;
+    f1out.open(filePath,std::ios::binary);
+    f1out.write((char*)this, sizeof(User));
+    f1out.close();
+}
+
+void User::readFromFile(char *filePath)
+{
+    std::ifstream f1in;
+    f1in.open(filePath,std::ios::binary);
+    f1in.read((char*)this, sizeof(User));
+    f1in.close();
 }
