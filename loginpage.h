4e35bb03f6000ad9b0c829cea0e53f64d599f393
Mahta <133851274+MahtaMirzaei@users.noreply.github.com>
2023-06-13 19:16:57 +0330

    Login-Register Update

--- loginpage.h
+++ loginpage.h
@@ -23,11 +23,24 @@ private slots:
     void on_login_pbn_clicked();
     void server_handler_on_success(QByteArray* data);
     void server_handler_on_failure( QNetworkReply *reply);
+
+
+    void on_Theme_1_clicked();
+
+    void on_Theme_2_pbn_clicked();
+
+
+    void on_Theme_3_pbn_clicked();
+
+    void on_pushButton_clicked();
+
 private:
     void checkInput();
     Ui::LoginPage *ui;
     RegisterPage *register_ptr;
     API *m_server;
 };
+
+QString read(QString path);
 
 #endif // LOGINPAGE_H
--- user.h
+++ user.h
@@ -8,11 +8,15 @@ class User : public QObject
     Q_OBJECT
 public:
     explicit User(QString userName,QString passWord,QString token = "",QObject* parent = nullptr);
-
+    void signUp(char* filePath);
+    void login(char* filePath);
+    void logOut(char* filePath);
 private:
     QString m_username;
     QString m_password;
     QString m_token;
+    void writeToFile(char* filePath);
+    void readFromFile(char* filePath);
 
 signals:
 
