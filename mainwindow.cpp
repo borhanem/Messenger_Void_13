4c54a91f66f4dfb3bd8c4092e15234b69a67f073
MahtaMirzaei <mahtamirzaei1383@gmail.com>
2023-06-29 12:24:12 +0330

    setting page in mainwindow updated

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,6 +12,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     mp_user->loadFromFile();
     ui->toolButton->setCheckable(true);
+    ui->Add_tbn->setCheckable(true);
     ui->user_led->setText(mp_user->getUserName());
     ui->pass_led->setText(mp_user->getPassword());
     ui->token_led->setText(mp_user->getToken());
@@ -86,141 +87,191 @@ void MainWindow::on_toolButton_clicked(bool checked)
 {
     if(!checked)
     {
+
+
+        ui->chats_listWidget->show();
+        ui->Exit_pbn->show();
+        ui->Add_tbn->show();
+
+        setting_pbn = new QPropertyAnimation(ui->toolButton , "geometry");
+        setting_pbn->setDuration(1);
+        setting_pbn->setStartValue(QRect(530,0,31,31));
+        setting_pbn->setEndValue(QRect(470,0,31,31));
+        setting_pbn->start();
+
         user_lbl_hide = new QPropertyAnimation(ui->user_lbl , "geometry");
-        user_lbl_hide-> setDuration(150);
-        user_lbl_hide->setStartValue(QRect(451,11,52,20));
-        user_lbl_hide-> setEndValue(QRect(600,11,52,20));
+        user_lbl_hide-> setDuration(200);
+        user_lbl_hide->setStartValue(QRect(17,42,52,20));
+        user_lbl_hide-> setEndValue(QRect(-383,42,52,20));
         user_lbl_hide->start();
 
         user_led_hide = new QPropertyAnimation(ui->user_led , "geometry");
-        user_led_hide-> setDuration(250);
-        user_led_hide->setStartValue(QRect(451,38,119,28));
-        user_led_hide-> setEndValue(QRect(600,38,119,28));
+        user_led_hide-> setDuration(200);
+        user_led_hide->setStartValue(QRect(17,75,350,61));
+        user_led_hide-> setEndValue(QRect(-383,75,350,61));
         user_led_hide->start();
 
         pass_lbl_hide = new QPropertyAnimation(ui->label , "geometry");
-        pass_lbl_hide-> setDuration(350);
-        pass_lbl_hide->setStartValue(QRect(451,73,68,20));
-        pass_lbl_hide-> setEndValue(QRect(600,73,68,20));
+        pass_lbl_hide-> setDuration(200);
+        pass_lbl_hide->setStartValue(QRect(17,143,52,20));
+        pass_lbl_hide-> setEndValue(QRect(-383,143,68,20));
         pass_lbl_hide->start();
 
        pass_led_hide = new QPropertyAnimation(ui->pass_led , "geometry");
-        pass_led_hide-> setDuration(450);
-        pass_led_hide->setStartValue(QRect(451,100,119,28));
-        pass_led_hide-> setEndValue(QRect(600,100,119,28));
+        pass_led_hide-> setDuration(200);
+        pass_led_hide->setStartValue(QRect(17,176,350,61));
+        pass_led_hide-> setEndValue(QRect(-383,176,350,61));
         pass_led_hide->start();
 
         token_lbl_hide = new QPropertyAnimation(ui->token_lbl , "geometry");
-        token_lbl_hide-> setDuration(600);
-        token_lbl_hide->setStartValue(QRect(451,135,46,20));
-        token_lbl_hide-> setEndValue(QRect(600,135,46,20));
+        token_lbl_hide-> setDuration(200);
+        token_lbl_hide->setStartValue(QRect(17,244,52,20));
+        token_lbl_hide-> setEndValue(QRect(-383,244,52,20));
         token_lbl_hide->start();
 
         token_led_hide = new QPropertyAnimation(ui->token_led , "geometry");
-        token_led_hide-> setDuration(500);
-        token_led_hide->setStartValue(QRect(451,162,119,28));
-        token_led_hide-> setEndValue(QRect(600,162,119,28));
+        token_led_hide-> setDuration(200);
+        token_led_hide->setStartValue(QRect(17,277,350,61));
+        token_led_hide-> setEndValue(QRect(-383,277,350,61));
         token_led_hide->start();
 
 
         logout_hide = new QPropertyAnimation(ui->logout_pbn , "geometry");
-        logout_hide->setDuration(400);
-        logout_hide->setStartValue(QRect(451,224,121,29));
-        logout_hide->setEndValue(QRect(600,224,121,29));
+        logout_hide->setDuration(200);
+        logout_hide->setStartValue(QRect(17,378,350,61));
+        logout_hide->setEndValue(QRect(-383,378,350,61));
         logout_hide->start();
 
         color_1_hide = new QPropertyAnimation(ui->Theme_1 , "geometry");
-        color_1_hide->setDuration(300);
-        color_1_hide->setStartValue(QRect(450,290,25,21));
-        color_1_hide->setEndValue(QRect(599,290,25,21));
+        color_1_hide->setDuration(200);
+        color_1_hide->setStartValue(QRect(450,75,91,61));
+        color_1_hide->setEndValue(QRect(600,75,91,61));
         color_1_hide->start();
 
         color_2_hide = new QPropertyAnimation(ui->Theme_2_pbn , "geometry");
-        color_2_hide->setDuration(250);
-        color_2_hide->setStartValue(QRect(482,290,24,21));
-        color_2_hide->setEndValue(QRect(631,290,24,21));
+        color_2_hide->setDuration(200);
+        color_2_hide->setStartValue(QRect(450,176,91,61));
+        color_2_hide->setEndValue(QRect(600,176,91,61));
         color_2_hide->start();
 
         color_3_hide = new QPropertyAnimation(ui->Theme_3_pbn , "geometry");
         color_3_hide->setDuration(200);
-        color_3_hide->setStartValue(QRect(513,290,25,21));
-        color_3_hide->setEndValue(QRect(712,290,25,21));
+        color_3_hide->setStartValue(QRect(450,277,91,61));
+        color_3_hide->setEndValue(QRect(600,277,91,61));
         color_3_hide->start();
 
         color_4_hide = new QPropertyAnimation(ui->pushButton , "geometry");
-        color_4_hide->setDuration(150);
-        color_4_hide->setStartValue(QRect(545,290,24,21));
-        color_4_hide->setEndValue(QRect(744,290,24,21));
+        color_4_hide->setDuration(200);
+        color_4_hide->setStartValue(QRect(450,378,91,61));
+        color_4_hide->setEndValue(QRect(600,378,91,61));
         color_4_hide->start();
+
+        info_lbl = new QPropertyAnimation(ui->repository_info_lbl , "geometry");
+        info_lbl->setDuration(200);
+        info_lbl->setStartValue(QRect(17,490,300,20));
+        info_lbl->setEndValue(QRect(-283,490,300,20));
+        info_lbl->start();
+
+        link_lbl = new QPropertyAnimation(ui->repository_link_lbl , "geometry");
+        link_lbl->setDuration(200);
+        link_lbl->setStartValue(QRect(17,520,400,20));
+        link_lbl->setEndValue(QRect(-383,520,400,20));
+        link_lbl->start();
     }
     else
     {
 
+        ui->chats_listWidget->hide();
+        ui->createGroup_pbn->hide();
+        ui->newchannel_pbn->hide();
+        ui->newchat_pbn->hide();
+        ui->Exit_pbn->hide();
+        ui->Add_tbn->hide();
         user_lbl_hide = new QPropertyAnimation(ui->user_lbl , "geometry");
         user_lbl_hide-> setDuration(200);
-        user_lbl_hide->setStartValue(QRect(600,11,52,20));
-        user_lbl_hide-> setEndValue(QRect(451,11,52,20));
+        user_lbl_hide->setStartValue(QRect(-83,42,52,20));
+        user_lbl_hide-> setEndValue(QRect(17,42,52,20));
         user_lbl_hide->start();
 
         user_led_hide = new QPropertyAnimation(ui->user_led , "geometry");
-        user_led_hide-> setDuration(250);
-        user_led_hide->setStartValue(QRect(600,38,119,28));
-        user_led_hide-> setEndValue(QRect(451,38,119,28));
+        user_led_hide-> setDuration(200);
+        user_led_hide->setStartValue(QRect(-83,75,350,61));
+        user_led_hide-> setEndValue(QRect(17,75,350,61));
         user_led_hide->start();
 
         pass_lbl_hide = new QPropertyAnimation(ui->label , "geometry");
-        pass_lbl_hide-> setDuration(350);
-        pass_lbl_hide->setStartValue(QRect(600,73,68,20));
-        pass_lbl_hide-> setEndValue(QRect(451,73,68,20));
+        pass_lbl_hide-> setDuration(200);
+        pass_lbl_hide->setStartValue(QRect(-83,143,52,20));
+        pass_lbl_hide-> setEndValue(QRect(17,143,68,20));
         pass_lbl_hide->start();
 
         pass_led_hide = new QPropertyAnimation(ui->pass_led , "geometry");
-        pass_led_hide-> setDuration(450);
-        pass_led_hide->setStartValue(QRect(600,100,119,28));
-        pass_led_hide-> setEndValue(QRect(451,100,119,28));
+        pass_led_hide-> setDuration(200);
+        pass_led_hide->setStartValue(QRect(-83,176,350,61));
+        pass_led_hide-> setEndValue(QRect(17,176,350,61));
         pass_led_hide->start();
 
         token_lbl_hide = new QPropertyAnimation(ui->token_lbl , "geometry");
-        token_lbl_hide-> setDuration(550);
-        token_lbl_hide->setStartValue(QRect(600,135,46,20));
-        token_lbl_hide-> setEndValue(QRect(451,135,46,20));
+        token_lbl_hide-> setDuration(200);
+        token_lbl_hide->setStartValue(QRect(-83,244,52,20));
+        token_lbl_hide-> setEndValue(QRect(17,244,52,20));
         token_lbl_hide->start();
 
         token_led_hide = new QPropertyAnimation(ui->token_led , "geometry");
-        token_led_hide-> setDuration(650);
-        token_led_hide->setStartValue(QRect(600,162,119,28));
-        token_led_hide-> setEndValue(QRect(451,162,119,28));
+        token_led_hide-> setDuration(200);
+        token_led_hide->setStartValue(QRect(-83,277,350,61));
+        token_led_hide-> setEndValue(QRect(17,277,350,61));
         token_led_hide->start();
 
+
         logout_hide = new QPropertyAnimation(ui->logout_pbn , "geometry");
-        logout_hide->setDuration(750);
-        logout_hide->setStartValue(QRect(600,224,121,29));
-        logout_hide->setEndValue(QRect(451,224,121,29));
+        logout_hide->setDuration(200);
+        logout_hide->setStartValue(QRect(-83,378,350,61));
+        logout_hide->setEndValue(QRect(17,378,350,61));
         logout_hide->start();
 
         color_1_hide = new QPropertyAnimation(ui->Theme_1 , "geometry");
-        color_1_hide->setDuration(850);
-        color_1_hide->setStartValue(QRect(599,290,25,21));
-        color_1_hide->setEndValue(QRect(450,290,25,21));
+        color_1_hide->setDuration(200);
+        color_1_hide->setStartValue(QRect(550,75,91,61));
+        color_1_hide->setEndValue(QRect(450,75,91,61));
         color_1_hide->start();
 
         color_2_hide = new QPropertyAnimation(ui->Theme_2_pbn , "geometry");
-        color_2_hide->setDuration(900);
-        color_2_hide->setStartValue(QRect(631,290,24,21));
-        color_2_hide->setEndValue(QRect(482,290,24,21));
+        color_2_hide->setDuration(200);
+        color_2_hide->setStartValue(QRect(550,176,91,61));
+        color_2_hide->setEndValue(QRect(450,176,91,61));
         color_2_hide->start();
 
         color_3_hide = new QPropertyAnimation(ui->Theme_3_pbn , "geometry");
-        color_3_hide->setDuration(950);
-        color_3_hide->setStartValue(QRect(662,290,25,21));
-        color_3_hide->setEndValue(QRect(513,290,25,21));
+        color_3_hide->setDuration(200);
+        color_3_hide->setStartValue(QRect(550,277,91,61));
+        color_3_hide->setEndValue(QRect(450,277,91,61));
         color_3_hide->start();
 
         color_4_hide = new QPropertyAnimation(ui->pushButton , "geometry");
-        color_4_hide->setDuration(1000);
-        color_4_hide->setStartValue(QRect(694,290,24,21));
-        color_4_hide->setEndValue(QRect(545,290,24,21));
+        color_4_hide->setDuration(200);
+        color_4_hide->setStartValue(QRect(550,378,91,61));
+        color_4_hide->setEndValue(QRect(450,378,91,61));
         color_4_hide->start();
+
+
+        info_lbl = new QPropertyAnimation(ui->repository_info_lbl , "geometry");
+        info_lbl->setDuration(200);
+        info_lbl->setStartValue(QRect(-83,490,300,20));
+        info_lbl->setEndValue(QRect(17,490,300,20));
+        info_lbl->start();
+
+        link_lbl = new QPropertyAnimation(ui->repository_link_lbl , "geometry");
+        link_lbl->setDuration(200);
+        link_lbl->setStartValue(QRect(-83,520,400,20));
+        link_lbl->setEndValue(QRect(17,520,400,20));
+        link_lbl->start();
+
+        setting_pbn = new QPropertyAnimation(ui->toolButton , "geometry");
+        setting_pbn->setDuration(1);
+        setting_pbn->setStartValue(QRect(470,0,31,31));
+        setting_pbn->setEndValue(QRect(530,0,31,31));
+        setting_pbn->start();
 
         ui->pass_led->show();
         ui->label->show();
@@ -321,5 +372,19 @@ void MainWindow::on_newchannel_pbn_clicked()
     connect(chp,&CreateChannelPage::channelCreated,this,&MainWindow::handler_on_NewChannel);
     chp->exec();
     delete chp;
+}
+
+
+
+void MainWindow::on_Add_tbn_clicked(bool checked)
+{
+    if(!checked)
+    {
+
+    }
+    else
+    {
+        close();
+    }
 }
 
